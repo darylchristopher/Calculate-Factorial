6fb8d14ba5885b4f24efc206c28e2bf8b238d9db
Daryl Christopher <139321073+darylchristopher@users.noreply.github.com>
2024-11-28 11:01:45 +0530

    Update Calculate Factorial.py

--- Calculate Factorial.py
+++ Calculate Factorial.py
@@ -1,5 +1,10 @@
-num=int(input("Enter a Number: "))
-fact = 1
-for i in range(1,num+1):
-    fact=fact*i
-print("Factorial of",num," is ",fact)
+num=int(input("Enter a number: "))
+factorial = 1
+if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   for i in range(1,num + 1):
+       factorial = factorial*i
+   print("The factorial of",num,"is",factorial)
